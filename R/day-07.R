bb1c9c91cc256ec0980dc926044fe82d46204f53
chelsey su <chelsey5620@gmail.com>
2020-08-13 17:12:23 -0700

    pushing day-07 exercise

--- R/day-07.R
+++ R/day-07.R
@@ -21,21 +21,15 @@ right_join(covid, region, by = "state") %>%
 covid %>%
   right_join(region, by = "state") %>%
   group_by(region, date) %>%
-  summarize(cases  = sum(cases),
-            deaths = sum(deaths)) %>%
-  pivot_longer(cols = c('cases', 'deaths')) ->
-  covid_region
+  summarize(cases  = sum(cases), deaths = sum(deaths)) %>%
+  pivot_longer(cols = c('cases', 'deaths')) -> covid_region
 
 ggplot(covid_region, aes(x = date, y = value)) +
   geom_line(aes(col = region)) +
   facet_grid(name~region, scale = "free_y") +
-  theme_linedraw() +
+  theme_dark() +
   theme(legend.position = "bottom") +
   theme(legend.position = "NA") +
-  labs(title = "Cummulative Cases and Deaths: Region",
-       y = "Daily Cumulative Count",
-       x = "Date",
-       caption = "Daily Exercise 07",
-       subtitle = "COVID-19 Data: NY-Times" )
+  labs(title = "Cummulative Cases and Deaths: Region", y = "Daily Cumulative Count", x = "Date", caption = "Daily Exercise 07", subtitle = "COVID-19 Data: NY-Times" )
 
 ggsave(file = "img/day-07.png")
